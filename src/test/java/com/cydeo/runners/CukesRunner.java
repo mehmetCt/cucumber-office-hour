447749cef2f251d108e5d3f8b0da9cdbd757b4b1
mehmetCt <mehmet@cybertekschool.com>
2023-02-26 12:11:17 -0500

    json plugin is added

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html:target/cucumber-reports.html"
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber.json"
 
         },
         features = "src/test/resources/features",
